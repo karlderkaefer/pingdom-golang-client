181f18ce0d4736293c5ac6a6ca3a6baa9910b551
karlderkaefer <karlderkaefer@users.noreply.github.com>
2024-01-21 16:22:20 +0100

    fix: add pointer util

---
+++ pkg/pingdom/openapi/ptr/pointer.go
@@ -0,0 +1,29 @@
+package ptr
+
+func Bool(x bool) *bool {
+	return &x
+}
+
+func Int8(x int8) *int8 {
+	return &x
+}
+
+func Int16(x int16) *int16 {
+	return &x
+}
+
+func Int32(x int32) *int32 {
+	return &x
+}
+
+func Int64(x int64) *int64 {
+	return &x
+}
+
+func Float64(x float64) *float64 {
+	return &x
+}
+
+func String(x string) *string {
+	return &x
+}
